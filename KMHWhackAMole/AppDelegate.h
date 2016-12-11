417ec062f7df461181284879b768de8e944ea284
kenmhaggerty <kenmhaggerty@gmail.com>
2016-12-11 15:30:03 -0500

    Creates Xcode project.

---
+++ KMHWhackAMole/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KMHWhackAMole
+//
+//  Created by Ken M. Haggerty on 12/11/16.
+//  Copyright © 2016 Ken M. Haggerty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KMHWhackAMole/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KMHWhackAMole
+//
+//  Created by Ken M. Haggerty on 12/11/16.
+//  Copyright © 2016 Ken M. Haggerty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
